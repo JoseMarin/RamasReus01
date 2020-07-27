7fb91a27a1a340469b354eb2207a3196c193e0da
Jose <jmarinmalagon@gmail.com>
2020-07-27 15:47:33 +0200

    Dev2 primer update

--- RamasReus01/src/mainApp.java
+++ RamasReus01/src/mainApp.java
@@ -3,7 +3,8 @@ public class mainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo!!!");
+		String mens= "Adios!!";
+		System.out.println(mens);
 	}
 
 }
